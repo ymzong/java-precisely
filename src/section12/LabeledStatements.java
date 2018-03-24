47967634c7dab8245791853753855cbcee7f183a
Yiming Zong <ymzong@users.noreply.github.com>
2018-03-23 23:59:28 -0400

    complete labeled statements

---
+++ src/section12/LabeledStatements.java
@@ -0,0 +1,37 @@
+package section12;
+
+/**
+ * This class demonstrates the use of labeled statements (usually not recommended).
+ */
+public class LabeledStatements {
+
+    public static void main(String[] argv) {
+        outer: {
+            String line = "";
+            while (true) {
+                line += "*";
+                System.out.println(line);
+
+                if (line.length() > 5) {
+                    break outer;    // In this case, same as using `break` itself
+                }
+            }
+        }
+
+        // Execution continues here after `break outer`
+        String s1 = "123javascript";
+        String s2 = "java";
+        outer:
+            for (int start = 0; start + s2.length() < s1.length(); start++) {   // start index for substring
+                for (int idx = 0; idx < s2.length(); idx++) {
+                    if (s1.charAt(start + idx) != s2.charAt(idx)) {
+                        continue outer;
+                    }
+                }
+                System.out.format("match found with start=%d.%n", start);
+                System.exit(0);
+            }
+        System.out.println("match not found!");
+    }
+
+}
