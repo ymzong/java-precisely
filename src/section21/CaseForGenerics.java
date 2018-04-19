7c89b6f74d61315d62247bf9f7e0eeeeb5c6b3fb
Yiming Zong <ymzong@users.noreply.github.com>
2018-04-18 22:47:20 -0400

    add quick example of benefits of generics

---
+++ src/section21/CaseForGenerics.java
@@ -0,0 +1,31 @@
+package section21;
+
+import java.util.ArrayList;
+
+/**
+ * The examples here shows how generics allows for stronger type-checking and more expressive code.
+ */
+public class CaseForGenerics {
+
+    public static void main(String[] argv) {
+        /* ArrayList without generics, where every entry is of Object type */
+        ArrayList names = new ArrayList();
+        names.add("Peter");
+        names.add("Jimmy");
+        names.add(42);      // no compile-time error here
+        try {
+            // no compile-time error here, but it causes ClassCastException at runtime
+            String name = (String) names.get(2);
+        } catch (ClassCastException e) {
+            System.out.println("oops");
+        }
+
+        /* ArrayList with generics, where the code is much cleaner and safer */
+        ArrayList<String> coolList = new ArrayList<String>();
+        coolList.add("Peter");
+        coolList.add("Jimmy");
+        //coolList.add(42);             // compile-time error would be triggered due to type mismatch
+        String name = coolList.get(1);  // guaranteed to be safe thanks to generics
+    }
+
+}
