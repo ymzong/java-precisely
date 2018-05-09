3042490974e48ae396503e566df4a2183538daa8
Yiming Zong <ymzong@users.noreply.github.com>
2018-05-09 02:01:23 -0400

    add quicksort method that uses default comparator

--- src/section21/GenericQuickSort.java
+++ src/section21/GenericQuickSort.java
@@ -13,6 +13,16 @@ public class GenericQuickSort {
         quickSort(arr, cmp, 0, arr.length - 1);
     }
 
+    /* Quicksorts the entire array using the default comparator from element type */
+    public static <T extends Comparable<T>> void quickSort(T[] arr) {
+        // hacky implementation that constructs the default Comparator on the spot
+        quickSort(arr, new Comparator<T>() {
+            public int compare(T x, T y) {
+                return x.compareTo(y);
+            }
+        });
+    }
+
     /* Quicksorts the array using the given comparator between start and end index (inclusive) */
     private static <T> void quickSort(T[] arr, Comparator<T> cmp, int start, int end) {
         if (start >= end) {     // empty or singleton case
@@ -43,6 +53,7 @@ public class GenericQuickSort {
     }
 
     public static void main(String[] argv) {
+        // Comparator for descending order
         Comparator<Integer> desc = new Comparator<Integer>() {
             public int compare(Integer x, Integer y) {
                 return y - x;
@@ -50,8 +61,20 @@ public class GenericQuickSort {
         };
 
         Integer[] arr = new Integer[] { 2, 19, 54, -14, 5, 19, 9351, 1024 };
-        quickSort(arr, desc);   // sort the array in descending order
-
+        quickSort(arr, desc);                                   // invoke generic method without type parameter
         System.out.println(Arrays.toString(arr));
+
+        // Comparator for ascending order by the last digit
+        Comparator<Integer> lastDigitAsc = new Comparator<Integer>() {
+            public int compare(Integer x, Integer y) {
+                return Math.abs(x) % 10 - Math.abs(y) % 10;
+            }
+        };
+
+        GenericQuickSort.<Integer>quickSort(arr, lastDigitAsc); // invoke generic method with type parameter
+        System.out.println(Arrays.toString(arr));
+
+        GenericQuickSort.quickSort(arr);
+        System.out.println(Arrays.toString(arr));               // invoke quicksort with default comparator
     }
 }
